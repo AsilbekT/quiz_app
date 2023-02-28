a8b6c62865c9194b1044d986cdb86744079b42ba
Asilbek Turgunboev <asilbekturgunboev@Asilbeks-MacBook-Pro.local>
2023-02-28 23:19:43 +0500

    dockerizing the app

--- main_logic/views.py
+++ main_logic/views.py
@@ -29,56 +29,6 @@ def get_teams(request) -> Response:
 
     This function retrieves the data of all teams stored in the database. The returned data includes the name of the team,
     the leader's ID, the team's avatar URL, and a list of team members' IDs, names, and avatar URLs.
-
-    Returns:
-        JSON: A JSON response containing the following information:
-            team_name (str): The name of the team.
-            team_leader (int): The leader's ID.
-            team_avatar (str): The URL of the team's avatar.
-            members (list): A list of dictionaries, each containing the following information for a team member:
-                id (int): The team member's ID.
-                name (str): The team member's name.
-                avatar (str): The URL of the team member's avatar.
-
-    Example:
-        Request: GET /api/teams/
-        Response:
-        [
-            {
-                "team_name": "Team A",
-                "team_leader": 1,
-                "team_avatar": "https://example.com/team-a-avatar.jpg",
-                "members": [
-                    {
-                        "id": 1,
-                        "name": "John Doe",
-                        "avatar": "https://example.com/john-doe-avatar.jpg"
-                    },
-                    {
-                        "id": 2,
-                        "name": "Jane Doe",
-                        "avatar": "https://example.com/jane-doe-avatar.jpg"
-                    }
-                ]
-            },
-            {
-                "team_name": "Team B",
-                "team_leader": 3,
-                "team_avatar": "https://example.com/team-b-avatar.jpg",
-                "members": [
-                    {
-                        "id": 3,
-                        "name": "John Smith",
-                        "avatar": "https://example.com/john-smith-avatar.jpg"
-                    },
-                    {
-                        "id": 4,
-                        "name": "Jane Smith",
-                        "avatar": "https://example.com/jane-smith-avatar.jpg"
-                    }
-                ]
-            }
-        ]
     """
     teams = Team.objects.all()
     serializer = TeamsSerializer(teams, many=True)
@@ -92,11 +42,6 @@ def add_team(request):
     Add a new team using the POST method.
 
     This function is used to create a new team.
-    Args:   
-        team_name,
-        team_leader,
-        team_avatar,
-        members
     """
     serializer = TeamSerializer(data=request.data)
     if serializer.is_valid():
@@ -117,52 +62,6 @@ def get_or_update_team(request, id) -> Response:
     To delete a team, send a DELETE request.
     The required parameters for either getting or updating a team are the name of the team and the ID of the team leader.
     Optionally, a list of IDs for team members can be included in the request.
-
-    Args:
-        team_name (str): The name of the team.
-        team_leader (int): An account ID for the leader of the team.
-        members (list, optional): A list of account IDs for team members.
-
-    Returns:
-        JSON: A JSON response indicating the result of the request, with the following format:
-            Data:
-                id (int),
-                team_name (str),
-                team_leader (int),
-                team_avatar (str),
-                members (list).
-            message (str):
-                if a new team was created, "updated" if an existing team was updated,
-                or "deleted" if the team was deleted.
-
-    Examples:
-    PUT Request: /api/teams/<id>/get-or-update/
-    Data:
-        {
-            "team_name": "Team A",
-            "team_leader": 1,
-            "members": [2, 3]
-        }
-    Response:
-        {
-            "message": "created"
-        }
-
-    GET Request: /api/teams/<id>/get-or-update/
-    Response:
-    {
-        "id": 8,
-        "team_name": "Team A",
-        "team_leader": 1,
-        "team_avatar": "/teams.jpg",
-        "members": [2, 3]
-    }
-
-    DELETE Request: /api/teams/<id>/get-or-update/
-    Response:
-    {
-        "message": "deleted"
-    }
     """
 
     try:
